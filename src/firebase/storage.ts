e94d87a2250401bd7a21e6017bf0d90df6f5bdf6
spellew <spellew1298@gmail.com>
2020-09-29 17:37:49 -0400

    Add query functionality to getDocuments

--- src/firebase/storage.ts
+++ src/firebase/storage.ts
@@ -2,8 +2,11 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 
 
-const getDocuments = (collection: string): Promise<firebase.firestore.DocumentData[]> => new Promise((resolve, reject) => {
-  firebase.firestore().collection(collection).get()
+const getDocuments = (collection: string, filter: { fieldPath: string, opStr: firebase.firestore.WhereFilterOp, value: any } | undefined = undefined): Promise<firebase.firestore.DocumentData[]> => new Promise((resolve, reject) => {
+  let ref: firebase.firestore.Query<firebase.firestore.DocumentData> | firebase.firestore.CollectionReference<firebase.firestore.DocumentData> = firebase.firestore().collection(collection);
+  if (filter !== undefined) ref = ref.where(filter.fieldPath, filter.opStr, filter.value);
+  
+  ref.get()
     .then(snapshot => resolve(snapshot.docs.map(doc => doc.data())))
     .catch(error => {
       console.error(error.code, error.message);
