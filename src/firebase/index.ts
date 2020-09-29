576a943b123e9c76f6f66f528d28a7b544e7a04f
spellew <spellew1298@gmail.com>
2020-09-29 15:30:22 -0400

    Separated auth and initialize code

---
+++ src/firebase/index.ts
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+
+
+const initialize = () => {
+  firebase.initializeApp({
+    apiKey: "AIzaSyAHEDybBBGR97FXlPFNMMVId_2tiyby9Ps",
+    authDomain: "gr-calc.firebaseapp.com",
+    databaseURL: "https://gr-calc.firebaseio.com",
+    projectId: "gr-calc",
+    storageBucket: "gr-calc.appspot.com",
+    messagingSenderId: "792549088089",
+    appId: "1:792549088089:web:dc97154c17c6da87190a48"
+  });
+}
+
+export {
+  initialize,
+};
